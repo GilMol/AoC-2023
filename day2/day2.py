d580055f215ac2acbb6ca890108f5ff516ec4885
Gil <mil.g@protonmail.com>
2023-12-03 13:38:33 +0100

    Added day2 part1 solution

--- day2/day2.py
+++ day2/day2.py
@@ -1,14 +1,48 @@
 import re
 
 pattern = r"(?=(\d+:))\d+|(?=(\d+.\w+))\d+"
+teststring = "Game 14: 4 blue, 2 green; 19 blue; 6 red, 17 blue; 10 blue, 7 red; 1 green, 2 blue, 7 red"
 
 file = "E:\\repos\AoC\day2\input.txt"
 
+testmatch = re.finditer(pattern,teststring)
+        
 with open(file) as f:
     lines= f.readlines()
 
-result = []
+def CheckDiceBag(match):
+    # map string of input to get the max available dice
+    cubes = {
+        'red' : '12',
+        'green' : '13',
+        'blue' : '14'
+    }
+    onlyColorMatch = re.findall(r"\D+",match.group(2))[0]
+    nrOfCubes = cubes[onlyColorMatch.strip()]
+    print(f"{onlyColorMatch} with {nrOfCubes} and in the example are {int(match.group(0))}")
+    if int(nrOfCubes) < int(match.group(0)):
+        return False
+    return True
+
+sum = 0
+
+def CheckSum(match):
+    global sum
+    isValid = True
+    for m in match:
+        if m.group(2) is not None:
+            if isValid is True:
+                isValid= CheckDiceBag(m)
+        else:
+            id = int(m.group(0))
+    print(f"{isValid} {sum} {id}")
+    if isValid is True:
+        sum += id
+
+# CheckSum(testmatch)
+
 for l in lines:
-    match = re.search(pattern,l)
-    print(match.group(0))
-    print(match.group(1))
+    match = re.finditer(pattern,l)
+    CheckSum(match)
+
+print(sum)
